962d364500032ba5ac26d5a6b3bdfbb5e52144e6
johndawsonn <80231581+johndawsonn@users.noreply.github.com>
2021-03-08 16:03:26 +1000

    my first commit

---
+++ js/Script.js
@@ -0,0 +1,16 @@
+
+function copyrightpopup()
+{
+alert("This website was made by Yamak Balthasar in the span of one night.")
+
+}
+function cookiespopup()
+{
+var c=confirm("Do you wish to enable cookies? They provide better user experience. No personal information will be collected.")
+if(c==true)
+alert("You have enabled cookies!")
+else
+alert("You have not enabled cookies.")
+
+}
+
